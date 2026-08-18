c54e6dce79ac18af1624ca55d066925624b4b269
yash77443@gmail.com <yash77443@gmail.com>
2026-08-18 20:47:19 +0530

    Update API backend URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://edumanage-server-2wgg.onrender.com',
         changeOrigin: true,
       },
     },
