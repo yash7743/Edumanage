7086640d15c925a9b3b3ad2c7f634a87f6624ef3
yash77443@gmail.com <yash77443@gmail.com>
2026-08-18 19:57:51 +0530

    point api to render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://edumanage-server-2wgg.onrender.com/api',
   withCredentials: true, // send httpOnly JWT cookie automatically
   headers: { 'Content-Type': 'application/json' },
 });
